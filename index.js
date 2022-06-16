1d7ad1a8835f5e5ee1d01c2bb59790240fe75ff9
Nyanthie Mayang Gatluak <nyanthie.mayang@student.moringaschool.com>
2022-06-17 01:36:37 +0300

    commit tree 79935693944313977b60af21eb39446b77a6c67e

--- index.js
+++ index.js
@@ -1,17 +1,24 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
+  return fetch('https://anapioficeandfire.com/api/books')
+    .then(res => res.json())
+    .then(booksJSON => renderBooks(booksJSON))
+}
+
+
+
+function renderBooks(books) { 
+  var main = document.querySelector('main'); 
+  books.forEach(book => { 
+  var h2 = document.createElement('h2'); 
+  h2.innerHTML = book.name; 
+  var h1 = document.createElement('h1'); 
+  h1.innerText = book.character; 
+  h2.innerText = book.name; main.appendChild(h2); });
+
+  }
   
-}
-
-function renderBooks(books) {
-  const main = document.querySelector('main');
-  books.forEach(book => {
-    const h2 = document.createElement('h2');
-    h2.innerHTML = book.name;
-    main.appendChild(h2);
-  });
-}
-
-document.addEventListener('DOMContentLoaded', function() {
-  fetchBooks();
-});
+  document.addEventListener('DOMContentLoaded', function() {
+    document.addEventListener('DOMContentLoaded', function () {
+      fetchBooks();
+    })});
